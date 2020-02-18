6b255ecfd7480e091b5c4364dc088667ba6a4c70
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-02-18 17:49:33 +0000

    Function intro examples

---
+++ hello.js
@@ -0,0 +1,10 @@
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+const sayHelloname  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello();
+sayHelloname("Amro");
