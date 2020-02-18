3a7a74df4e31cd2bbc7c0a694063fc35073384ac
Amro Abdelaziz <amr.hamada93@gmail.com>
2020-02-18 20:48:45 +0000

    Website broken not working so submitting answer to github

---
+++ range.js
@@ -0,0 +1,18 @@
+function range(start, end , step){
+  let result = [];
+  if( start === undefined || end === undefined || step === undefined ){
+    return result ;
+  }else if ( start > end ){
+    return result ;
+  }else if ( step === 0 ){
+    return result;
+  }
+  for (let x = start; x <= end; x += step){
+    result.push(x);
+  }
+  return result;
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
